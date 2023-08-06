f032b3f841c2d17b289d3abf95b78c6b406133a7
rohan-nambiar <hellorohan.nambiar@gmail.com>
2023-08-06 00:20:33 -0400

    Added Form Control + Reset Button + List , By pass PR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todoForm: FormGroup;
+
+  todoList: number[] = [];
+  todoPrint: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.todoForm = this.formBuilder.group({
+      todo: ['', Validators.required],
+    });
+  }
   title = 'todo-app';
+
+  getToDoValue() {
+    this.todoList.push(this.todoForm.get('todo')?.value);
+    console.log(this.todoForm.get('todo'));
+    this.todoForm.get('todo')?.setValue(null);
+  }
+
+  printToDoValue() {
+    this.todoPrint = !this.todoPrint;
+  } 
+
+  resetToDoList() {
+    this.todoList = [];
+  }
+
+
+
+
 }
