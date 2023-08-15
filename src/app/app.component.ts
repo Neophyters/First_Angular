b07180307d4d5db5159fc0075ea722fe59e66c92
rohan-nambiar <117684655+rohan-nambiar@users.noreply.github.com>
2023-08-14 22:39:34 -0400

    Rohan/feature/merge checkbox (#6)
    * Added todo list checkbox toggle
    
    * Made a Checkbox
    
    * Combined Jason and Max Checkbox
    
    * Merged jason and max checkbox and implemented mat-checkbox
    
    ---------
    
    Co-authored-by: Max-Y-Ma <maxma2@illinois.edu>
    Co-authored-by: jasoncUD <jasonc@udel.comgit config --global user.email jasonc@udel.comgit config --global user.email jasonc@udel.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,16 +12,21 @@ export class AppComponent {
   todoList: number[] = [];
   todoPrint: boolean = false;
 
+  isChecked: boolean = false;
+
   constructor(private formBuilder: FormBuilder) {
     this.todoForm = this.formBuilder.group({
       todo: ['', Validators.required],
-    });
+      showTodoList: [false],
+    });    
   }
+  
   title = 'todo-app';
 
   getToDoValue() {
     this.todoList.push(this.todoForm.get('todo')?.value);
     this.todoForm.get('todo')?.setValue(null);
+    console.log(this.todoForm.get('showTodoList')?.value);
   }
 
   printToDoValue() {
@@ -32,7 +37,14 @@ export class AppComponent {
     this.todoList = [];
   }
 
-
-
-
+  checkBoxClicked() {
+    if (this.isChecked == true) {
+      this.isChecked = false;
+      document.getElementById("todolist")!.style.display = "none";
+    }
+    else {
+      this.isChecked = true;
+      document.getElementById("todolist")!.style.display = "block";
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -12,11 +15,14 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CheckboxModule,
+    BrowserAnimationsModule, 
+    MatCheckboxModule
   ],
 
   providers: [],
   
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
