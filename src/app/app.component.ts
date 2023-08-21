ae699389fd8614ae4b99c1472b1cb27ac4779991
rohan-nambiar <hellorohan.nambiar@gmail.com>
2023-08-21 05:37:29 -0400

    Merged User Login and Improved HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { CalendarEvent, CalendarView } from 'angular-calendar';
-import { startOfDay, addDays } from 'date-fns';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @Component({
   selector: 'app-root',
@@ -12,25 +11,35 @@ export class AppComponent {
   title = 'todo-app';
   todoForm: FormGroup;
   
+  displayLogin: boolean = false;
   todoList: number[] = [];
 
   constructor(private formBuilder: FormBuilder) {
     this.todoForm = this.formBuilder.group({
       todo: ['', Validators.required],
       showTodoList: [false],
+      listDate: [''],
     });    
   }
+ 
+  openUserLogin() {
+    this.displayLogin = !this.displayLogin;
+  }
   
-  
-
   getToDoValue() {
-    this.todoList.push(this.todoForm.get('todo')?.value);
-    this.todoForm.get('todo')?.setValue(null);
-    console.log(this.todoForm.get('listCalendar')?.value);
+    if (this.todoForm.get('todo')?.value) {
+      this.todoList.push(this.todoForm.get('todo')?.value);
+      this.todoForm.get('todo')?.setValue(null);
+    }
   }
 
   resetToDoList() {
     this.todoList = [];
+  }
+
+  addItem(event: any) {
+    event.preventDefault();
+    this.getToDoValue();
   }
 
 }
